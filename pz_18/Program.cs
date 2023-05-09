3c910f15438ef341966d8b9e77a8483e750e5ba2
Золин Никита <zolinnikita69@gmail.com>
2023-05-09 15:50:21 +0500

    Добавьте файлы проекта.

---
+++ pz_18/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    enum Marks: byte
+    {
+        ОченьПлохо = 1,
+        Неудовлетворительно,
+        Удовлетворительно,
+        Хорошо,
+        Отлично
+    }
+
+    enum Seasons: byte
+    {
+        Зима,
+        Весна,
+        Лето,
+        Осень
+    }
+
+    static void Main(string[] args)
+    {
+        // 1)
+
+        Console.Write("Введите оценку: ");
+        byte value = Convert.ToByte(Console.ReadLine());
+        Marks mark;
+
+        if (Enum.IsDefined(typeof(Marks), value))
+        {
+            mark = (Marks)value;
+            switch (mark)
+            {
+                case Marks.ОченьПлохо:
+                    Console.WriteLine($"Характеристика оценки: {mark}");
+                    break;
+                case Marks.Неудовлетворительно:
+                    Console.WriteLine($"Характеристика оценки: {mark}");
+                    break;
+                case Marks.Удовлетворительно:
+                    Console.WriteLine($"Характеристика оценки: {mark}");
+                    break;
+                case Marks.Хорошо:
+                    Console.WriteLine($"Характеристика оценки: {mark}");
+                    break;
+                case Marks.Отлично:
+                    Console.WriteLine($"Характеристика оценки: {mark}");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+            Console.WriteLine("такой оценки не существует");
+
+        // 2)
+
+        Console.Write("Введите сезон: ");
+        string str = Console.ReadLine();
+
+        if (str != null)
+        {
+            Seasons season = (Seasons)Enum.Parse(typeof(Seasons), str, ignoreCase: true);
+
+            switch (season)
+            {
+                case Seasons.Зима:
+                    Console.WriteLine("Праздники в данный промежуток: \nНовый год (31.12) \nРождество (07.01) \nДень защитника отечества (23.02) \nМасленица (28.02 - 06.03)");
+                    break;
+                case Seasons.Весна:
+                    Console.WriteLine("Праздники в данный промежуток: \n Международный женский день (08.03) \nПасха (24.04) \nДень труда (01.05) \nДень Победы (09.05)");
+                    break;
+                case Seasons.Лето:
+                    Console.WriteLine("Праздники в данный промежуток: \nМеждународный день защиты детей (01.06) \nДень России (12.06) \nДень святой троицы (12.06)");
+                    break;
+                case Seasons.Осень:
+                    Console.WriteLine("Праздники в данный промежуток: \n Восшествие на престол Е. И. В. Государя Императора Николая Александровича (20.10) \nДень народного единства (04.11)");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+            Console.WriteLine("Неверное значени");
+    }
+}
